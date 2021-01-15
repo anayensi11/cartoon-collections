4c870d31195d9ba54f238c021ae1d54c32b2976f
anayensi <vnvyensi@gmail.com>
2021-01-14 20:36:11 -0500

    commit tree 7f73435bb8eb3643948610e443e99effe81fae3a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,38 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+def roll_call_dwarves(dwarves)# code an argument here
+  # Your code here
+  dwarves.each_with_index do |x,y|
+    name = x
+    num = y+1
+    puts "#{num}. #{name}"
+  end
+end
+
+
+def summon_captain_planet(planeteer_calls)
+  # code an argument here
+  # Your code here
+  planeteer_calls.map do |x|
+    x.capitalize << "!"
+  end
+end
+
+
+def long_planeteer_calls(calls)# code an argument here
+  # Your code here
+  calls.any? do |x|
+    x.length > 4
+  end
+end
+
+
+def find_the_cheese(strings)# code an argument here
+  # the array below is here to help
+cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.find do |x|
+    cheese_types.include?(x)
+  end
+end
